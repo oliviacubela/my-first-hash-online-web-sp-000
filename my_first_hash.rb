30bf6ba5522d4abdf5e500cc1a8f7fbca4429d57
Olivia Cubela <oliviacubela@gmail.com>
2020-06-15 13:35:04 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,7 +17,7 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-puts shipping_manifest["oil paintings1"]
+return shipping_manifest["oil paintings"]
 
 end
 
