fc462f2ab1fe321c77405e610b49b2dc60e6bde4
Olivia Cubela <oliviacubela@gmail.com>
2020-06-15 13:36:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,10 +28,10 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
   shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
   # add 4 gun powder to the shipping_manifest hash below
-puts shipping_manifest
+return shipping_manifest
   # return the shipping_manifest hash below
 
 end
